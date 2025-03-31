090bec2fd923e6abd39728fb42ccd6b9426fd3b3
ccamel <camel.christophe@gmail.com>
2025-03-31 11:27:02 +0200

    feat(core): introduce routing behaviour module

---
+++ apps/event_sourcing_core/src/event_sourcing_core_mgr_behaviour.erl
@@ -0,0 +1,9 @@
+-module(event_sourcing_core_mgr_behaviour).
+
+-include_lib("event_sourcing_core.hrl").
+
+%% @doc Extracts the routing information from the command.
+%% The routing information is the aggregate module and the stream id.
+-callback extract_routing(Command :: command()) -> {ok, Route} | {error, Reason}
+    when Route :: {Aggregate :: module(), Id :: stream_id()},
+         Reason :: term().
