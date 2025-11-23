d37902e549f46447776744d2a113da724f39c913
ccamel <camel.christophe@gmail.com>
2025-11-23 19:22:07 +0100

    refactor(store_mnesia): make store_es a standalone OTP application

---
+++ apps/es_store_mnesia/src/es_store_mnesia_sup.erl
@@ -0,0 +1,15 @@
+-module(es_store_mnesia_sup).
+-behaviour(supervisor).
+
+-export([start_link/0]).
+-export([init/1]).
+
+-spec start_link() -> {ok, pid()} | {error, term()}.
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+-spec init([]) ->
+    {ok, {{supervisor:strategy(), non_neg_integer(), pos_integer()}, [supervisor:child_spec()]}}.
+init([]) ->
+    %% No processes to supervise; keep the application running
+    {ok, {{one_for_one, 1, 5}, []}}.
