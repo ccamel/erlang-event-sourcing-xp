8c829cc962e3f9b13bfbce93aaf269bbc2f16c7f
ccamel <camel.christophe@gmail.com>
2026-05-09 22:01:09 +0200

    feat(xp): wire projection runtime into demo app

--- apps/es_xp/src/es_xp_app.erl
+++ apps/es_xp/src/es_xp_app.erl
@@ -12,7 +12,15 @@
 start(_StartType, _StartArgs) ->
     ok = es_kernel_registry:register(bank_account, bank_account_aggregate),
 
-    es_xp_sup:start_link().
+    case es_xp_sup:start_link() of
+        {ok, Pid} ->
+            StoreContext = es_kernel_app:get_store_context(),
+            {ok, _ProjectionPid} =
+                es_projection:start(StoreContext, bank_account_balance_projection, #{}),
+            {ok, Pid};
+        Error ->
+            Error
+    end.
 
 stop(_State) ->
     ok.
